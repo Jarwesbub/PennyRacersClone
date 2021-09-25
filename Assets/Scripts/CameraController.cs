f4c68e9374391a4f3f8a3f78973517cb1b94ed93
Jarno <jarwes90@gmail.com>
2021-09-25 03:17:32 +0300

    Camera upgrade -> follow any car

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,20 +8,22 @@ public class CameraController : MonoBehaviour
     public GameObject CameraPosition; //Where camera "sits"
     public GameObject CameraLookAtPosition; //Where camera looks
     public GameObject BackCamera;
+    public GameObject AICars;
+
     public float GroundTime;
     public float AirTime;
     private int CamSet;
-
     Vector3 CameraOffset, FixedCameraPosition;
 
     private int LookCommander = 0;
-
+    private bool holdbutton = false;
     private void Awake()
     {
         CameraOffset = CameraPosition.transform.position;
         gameObject.transform.position = CameraOffset;
         CamSet = 1;
 
+        CameraFollow(-1);//player
 
     }
 
@@ -46,7 +48,90 @@ public class CameraController : MonoBehaviour
 
         }
     }
+    void Update()
+    {
+        if(Input.GetKey("enter"))
+        {
+            holdbutton = true;
+        }
+        else
+        {
+            holdbutton = false;
+        }
 
+        if (holdbutton == true)
+        {
+            if (Input.GetKey(KeyCode.Keypad9))//player
+            {
+                CameraFollow(-1);
+            }
+            else if (Input.GetKey(KeyCode.Keypad0))
+            {
+                CameraFollow(0);
+            }
+            else if (Input.GetKey(KeyCode.Keypad1))
+            {
+                CameraFollow(1);
+            }
+            else if (Input.GetKey(KeyCode.Keypad2))
+            {
+                CameraFollow(2);
+            }
+            else if (Input.GetKey(KeyCode.Keypad3))
+            {
+                CameraFollow(3);
+            }
+            else if (Input.GetKey(KeyCode.Keypad4))
+            {
+                CameraFollow(4);
+            }
+            else if (Input.GetKey(KeyCode.Keypad5))
+            {
+                CameraFollow(5);
+            }
+            else if (Input.GetKey(KeyCode.Keypad6))
+            {
+                CameraFollow(6);
+            }
+            else if (Input.GetKey(KeyCode.Keypad7))
+            {
+                CameraFollow(7);
+            }
+            else if (Input.GetKey(KeyCode.Keypad8))
+            {
+                CameraFollow(8);
+            }
+        }
+
+    }
+    private void CameraFollow(int number)
+    {
+        GameObject cameraposition;
+        GameObject cameralookatposition;
+        GameObject backcamera;
+        if (number == -1) // PLAYER
+        {
+            cameraposition = Player.transform.transform.GetChild(1).gameObject;
+            CameraPosition = cameraposition;
+            cameralookatposition = Player.transform.GetChild(2).gameObject;
+            CameraLookAtPosition = cameralookatposition;
+            backcamera = Player.transform.GetChild(3).gameObject;
+            BackCamera = backcamera;
+        }
+        else // AI
+        {
+            GameObject AInumb = AICars.transform.GetChild(number).gameObject;
+            cameraposition = AInumb.transform.GetChild(0).gameObject;
+            CameraPosition = cameraposition;
+            cameralookatposition = AInumb.transform.GetChild(1).gameObject;
+            CameraLookAtPosition = cameralookatposition;
+            backcamera = AInumb.transform.GetChild(2).gameObject;
+            BackCamera = backcamera;
+
+        }
+        CameraOffset = CameraPosition.transform.position;
+        gameObject.transform.position = CameraOffset;
+    }
 
     public void ChangeCameraSettings(int value)
     {
